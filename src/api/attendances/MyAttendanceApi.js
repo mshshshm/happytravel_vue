14adf522165bd51101495c4d1c5bd6285a83ece2
mshshshm <hysung2588@gmail.com>
2024-06-24 15:59:13 +0900

    Refactor: MyAttendance Api

--- src/api/attendances/MyAttendanceApi.js
+++ src/api/attendances/MyAttendanceApi.js
@@ -24,3 +24,18 @@ export const getVacation = async (empId) => {
     throw error;
   }
 };
+
+// POST
+export const insertAttendanceManagement = async (attendanceManagement) => {
+  try {
+    const response = await apiClient.post(
+      "/attendance/myAttendance",
+      attendanceManagement
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error inserting attendanceManagement:", error);
+    throw error;
+  }
+};
+
